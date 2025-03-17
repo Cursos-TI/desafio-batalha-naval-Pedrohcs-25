75d8360afdeb5efe2960a5b819c7f56d19a3a1dc
Pedrohcs-25 <202503455431@alunos.estacio.br>
2025-03-17 00:29:54 -0300

    Novato v1

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -9,6 +9,111 @@ int main() {
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    int tabuleiro[10][10] = {
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,9,0,9,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,8,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,8,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0},
+        {0,0,0,0,0,0,0,0,0,0}
+    };
+
+     
+    // navios [i][2][2]
+    // i - quantidade de navios
+    // [2] - [0] posição inicial  - [1] posição final 
+    // [2] - [0] posição  v - [1] posição  h
+    int navios [3][2][2] = {
+        {{2,2},{2,4}},
+        {{4,2},{6,2}},
+        {{9,2},{9,8}}
+    };
+    
+    // inserir navios
+    int w = 0;
+    //int d =0;
+    
+    while (1){
+        if ((navios[w][0][0] == 0 && navios[w][0][1] == 0)&&(navios[w][1][0] == 0 && navios[w][1][1] == 0)){
+            break;
+        }
+        
+        //printf("navios[%d][0][0] %d ",w, navios[w][0][0]);
+        //printf("navios[%d][0][1] %d ",w, navios[w][0][1]);
+        //calculo
+        int distanciaV, distanciaH, invertido = 1;
+        distanciaV = navios[w][1][0] - navios[w][0][0];
+        distanciaH = navios[w][1][1] - navios[w][0][1];
+        if (distanciaH <0)
+        {
+            distanciaH = distanciaH * -1;
+            invertido = -1;
+        }else if (distanciaV<0){
+            invertido = -1;
+            distanciaV = distanciaV * -1;
+        }
+        int tMeio = (distanciaH > distanciaV) ? distanciaH : distanciaV;
+        int meio[tMeio][2];
+        if (distanciaH > distanciaV && distanciaV ==0){
+            for (int d = 0; d < distanciaH; d++)
+            {
+                meio[d][0] = (navios[w][0][0]);
+                meio[d][1] = navios[w][0][1] + ( d +1 )* invertido;
+            }
+            
+        }else if(distanciaV > distanciaH && distanciaH == 0){
+            
+            
+            for (int  d = 0; d < distanciaV; d++)
+            {
+                meio[d][0] = navios[w][0][0] + ( d +1 )* invertido;
+                meio[d][1] = navios[w][0][1];
+            }
+            
+        }
+        
+        
+
+        ////////////////////
+        for (short int i = 0; i < 10; i++){
+            for (short int j = 0; j < 10; j++){
+              if ((navios[w][0][0] == i && navios[w][0][1] == j)|| (navios[w][1][0] == i && navios[w][1][1] == j)){
+                tabuleiro[i][j] = 3 ;
+                continue;
+              }else{
+                //int m = 0;
+                for (int m = 0; m < tMeio; m++){
+                    if (meio[m][0] == i && meio[m][1] == j)
+                    {
+                        tabuleiro[i][j] = 3 ;
+                        continue;
+                    }
+                }
+                
+              }
+              
+            }
+            
+            
+        }
+
+
+        w ++;
+    }
+    printf("\n\n");
+    //exibir tabuleiro
+    for (short int i = 0; i < 10; i++){
+        for (short int j = 0; j < 10; j++){
+            printf("%d ", tabuleiro[i][j]);
+        }
+        printf("\n");
+        
+    }
+    
 
     // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
     // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
@@ -17,7 +122,7 @@ int main() {
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
-    // Sugestão: Utilize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
+    // Sugestão: Utilforize estruturas de repetição aninhadas para preencher as áreas afetadas por essas habilidades no tabuleiro.
     // Sugestão: Exiba o tabuleiro com as áreas afetadas, utilizando 0 para áreas não afetadas e 1 para áreas atingidas.
 
     // Exemplos de exibição das habilidades:
